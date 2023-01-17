eda46396098bb128ad3a9af167078220d801895e
2fishx <122938262+2fishx@users.noreply.github.com>
2023-01-17 23:37:33 +0100

    title

--- src/Info.tsx
+++ src/Info.tsx
@@ -3,31 +3,17 @@ import { Typography } from "@mui/material";
 export const Info = () => {
   return (
     <>
-      <>
-        <Typography
-          variant="h6"
-          style={{ display: "inline-block", opacity: 0.7 }}
-        >
-          2fishx.
-        </Typography>
-        <Typography
-          variant="h4"
-          gutterBottom
-          style={{ display: "inline-block" }}
-        >
-          Nie chce mi się liczyć
-        </Typography>
-        <Typography
-          variant="h6"
-          style={{ display: "inline-block", opacity: 0.7 }}
-        >
-          .com
-        </Typography>
-      </>
+      <Typography
+        variant="h4"
+        gutterBottom
+      >
+        Konwerter donejtów, subów i bitsów na złotówki dla odlicznika Tipply
+      </Typography>
       <Typography variant="subtitle1" gutterBottom>
-        Wpisz w pole poniżej kwoty donejtów, bitsów i ilośc subów z odpowiednią
+        Wpisz w pole poniżej kwoty donejtów, bitsów i ilość subów z odpowiednią
         końcówką (z - złotych, b - bitsów, s - subów) i wciśnij enter lub guzik
-        obok by obliczyć liczbę złotych jaką trzeba dodać do licznika w Tipply.
+        obok by obliczyć liczbę złotych jaką trzeba dodać do licznika w Tipply
+        (kwota kopiuje się automatycznie do schowka).
         Poniżej jest też historia dodawanych kwot.
       </Typography>
       <Typography variant="body1">Przykłady:</Typography>
